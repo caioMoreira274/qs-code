a2ecba2a627abad515f6e431b20cfa4c6086a435
caioMoreira274 <101023520+caioMoreira274@users.noreply.github.com>
2023-05-16 00:09:00 +0000

    tdd calculadora método multiplicar 1 e 1

--- calculadora/calculadora_test.py
+++ calculadora/calculadora_test.py
@@ -51,6 +51,12 @@ class TestCalculadora(unittest.TestCase):
 
         self.assertEqual(saida,esperado) 
 
+    def test_multiplicar_1_1(self):
+        saida = Calculadora.multiplicar(x=1,y=1)
+        esperado = 1
+
+        self.assertEqual(saida,esperado) 
+
 
  
     
--- calculadora/math_samples.py
+++ calculadora/math_samples.py
@@ -8,5 +8,7 @@ class Calculadora:
         return x - y
 
     def multiplicar(x,y):
+        if x==1 and y==1:
+            return 1
         return 0
 
